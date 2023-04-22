67361f7f0e76ea3efc7f6319cc32b9e182283f39
Mickael Brunet <mickael.brunet64@gmail.com>
2023-04-22 13:18:18 +0200

    Changes in auth and user module

--- src/user/service/user/user.service.ts
+++ src/user/service/user/user.service.ts
@@ -2,26 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserDto } from 'src/user/dto/user.dto/user.dto';
 import { User } from 'src/user/user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private dataSource: DataSource,
   ) {}
 
-  create(user: UserDto) {
-    this.usersRepository.create(user);
-    return user;
+  async create(user: UserDto): Promise<UserDto> {
+    const userCreated = this.usersRepository.create(user);
+    await this.dataSource.transaction(async (manager) => {
+      await manager.save(userCreated);
+    });
+    return userCreated;
   }
 
-  findAll(): Promise<UserDto[]> {
+  findAll(): Promise<User[]> {
     return this.usersRepository.find();
   }
 
-  findOne(id: number): Promise<UserDto | null> {
+  findById(id: number): Promise<UserDto | null> {
     return this.usersRepository.findOneBy({ id });
+  }
+
+  findByUsername(firstName: string): Promise<UserDto | null> {
+    return this.usersRepository.findOneBy({ firstName });
   }
 
   async remove(id: number): Promise<void> {
